00858dd4c18a8dc94a1055c7260b12cd4c55718c
Dertunfisch2 <Dertunfisch2@gmail.com>
2025-08-13 20:02:08 +0200

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,9 @@
+Config = {
+    easeTime = 100,
+    leftRange = 1.0,
+    keymapping = {
+        name = "tunfisch_leftpeak",
+        description = "Toggle Leftpeak",
+        key = "H"
+    }
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+author 'Dertunfisch2 - https://discord.gg/G35Tc8jdZw'
+description 'Leftpeak Script'
+game 'gta5'
+lua54 'yes'
+
+escrow_ignore {
+    'config.lua'
+}
+
+client_scripts {
+    'config.lua',
+    'client/main.lua'
+}
